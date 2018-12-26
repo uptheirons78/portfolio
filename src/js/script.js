374cf008104e54c55d423bb983a42e60e45662c1
Mauro <bono.mauro@gmail.com>
2018-12-26 12:55:52 +0100

    add animations on scroll using sal.js

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,15 @@ import { toggle } from './modules/Menu';
 import { yearInTheFooter } from './modules/FooterDate';
 import Swiper from 'swiper';
 import swiper from './modules/SwipeMe';
+import sal from 'sal.js';
 
 toggle();
 yearInTheFooter();
+
+sal({
+  threshold: 0.5,
+  once: false,
+});
 
 
 
