eed6c9e68d279c9884adb6146cda27c6c575525f
Mauro <bono.mauro@gmail.com>
2018-12-28 11:45:25 +0100

    added jump js for smooth scrolling

--- src/js/script.js
+++ src/js/script.js
@@ -3,14 +3,26 @@ import { yearInTheFooter } from './modules/FooterDate';
 import Swiper from 'swiper';
 import swiper from './modules/SwipeMe';
 import sal from 'sal.js';
+import jump from 'jump.js';
 
 toggle();
 yearInTheFooter();
 
+// sal js 
 sal({
   threshold: 0.5,
   once: false,
 });
+
+// jump js
+const btnOne = document.querySelector('#btn-1');
+const btnTwo = document.querySelector('#btn-2');
+const btnThree = document.querySelector('#btn-3');
+
+btnOne.addEventListener('click', () => jump('#portfolio', {duration:2000}));
+btnTwo.addEventListener('click', () => jump('#contact', {duration:2000}));
+btnThree.addEventListener('click', () => jump('#contact', {duration:2000}));
+
 
 
 
